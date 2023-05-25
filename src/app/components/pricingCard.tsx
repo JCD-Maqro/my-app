fbbd75179b0fd0516d7df84cb8e566934cd995e2
JCD-Maqro <carlosdulos@gmail.com>
2023-05-25 11:53:02 +1000

    edited devices in the pricing card

--- src/app/components/pricingCard.tsx
+++ src/app/components/pricingCard.tsx
@@ -78,7 +78,7 @@ const PricingCard: FC<PricingCardProps> = ({title, isMonthly}) => {
                             <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"/>
                             </svg>
                             <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 xxs:text-sm">
-                            5 Devices
+                            {numDevice} Devices
                             </span>
                         </div>
                         <div className="flex space-x-3">
