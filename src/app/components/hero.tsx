0ace57237eae5e762042b2b3d7723740b67e27a1
JCD-Maqro <carlosdulos@gmail.com>
2023-05-30 09:06:05 +1000

    Changes in the hero section

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     return (
         <section className="flex flex-col items-center justify-center h-auto w-auto mb-12 xxs:text-xs text-center">
             
-            <div className='bg-halfmoon bg-cover z-1 xxs:justify-center items-center'>
+            <div className='bg-halfmoon bg-cover xxs:justify-center items-center'>
                 <h1 className="shadow-2xl- md:text-3xl font-bold pt-10 text-white xxs:text-lg">Thriving in Digital Tranquility:  </h1>
                 <p className="uppercase font-extrabold shadow-2xl drop-shadow-xl lg:text-lg mb-8 text-pink-400 sm:text-xs">
                     Cyber Sakura, Your Security Oasis. 
