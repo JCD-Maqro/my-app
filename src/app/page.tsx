882e4f58b85b93395466955f5a3a70bd4962e943
JCD-Maqro <carlosdulos@gmail.com>
2023-05-29 16:22:15 +1000

    edited the size of the cherry blossom gif

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
     <div className='flex flex-col min-h-screen'>
     <Navbar />
     <div className='min-h-screen flex flex-col justify-center text-center items-center bg-bg3 bg-fixed bg-no-repeat bg-cover'>
-      <div className='bg-blossom bg-fixed bg-fit bg-cover w-[80vw] md:h-screen xxs:h-auto'>
+      <div className='bg-blossom bg-fixed bg-fit bg-cover w-[90vw] md:h-screen xxs:h-auto'>
         <Hero />
         <div className='flex justify-center mt-32 px-6 xxs:mt-2'>
           <Trustedby />
