a6266e40d38de612e6a592b925c27a7efb751bdb
JCD-Maqro <carlosdulos@gmail.com>
2023-06-01 15:54:35 +1000

    edited homepage

--- src/app/components/featuresCard.tsx
+++ src/app/components/featuresCard.tsx
@@ -10,13 +10,13 @@ interface FeaturesCardProps {
 }
 
 const FeaturesCard: FC<FeaturesCardProps> = ({ title, image, content, isFlip }) => {
-  const imgOrder = isFlip ? 'md:order-1 pb-3 lg:mr-24' : 'md:order-2 pb-3 lg:ml-24';
+  const imgOrder = isFlip ? 'md:order-1  lg:mr-24' : 'md:order-2  lg:ml-24';
   const conOrder = isFlip ? 'md:order-2' : 'md:order-1';
 
   return (
     <section className="w-[99%] bg-gray-800 gap-32 lg:p-6 lg:inline-flex lg:flex-wrap lg:justify-center xxs:p-0">
       <div className={`${imgOrder} xxs:m-0`}>
-        <Image src={image} alt={title} height={400} className="mx-auto" />
+        <Image src={image} alt={title} height={500} className="mx-auto" />
       </div>
       <div className={`text-left p-6 flex flex-col justify-center ${conOrder}`}>
         <h3 className="text-white text-xl font-bold mb-4 md:text-left sm:text-center xxs:text-center">{title}</h3>
